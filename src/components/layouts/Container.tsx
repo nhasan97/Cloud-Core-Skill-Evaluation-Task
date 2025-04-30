59b627deb5fef72e58940b6c09bf628fb913aa10
nhasan97 <nh6069325@gmail.com>
2025-04-30 20:56:07 +0600

    removed border from container and updated path in tsconfig.json

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Container = ({ children }: TChildren) => {
   return (
-    <div className="max-w-screen-2xl mx-auto px-5 md:px-8 lg:px-10 border">
+    <div className="max-w-screen-2xl mx-auto px-5 md:px-8 lg:px-10">
       {children}
     </div>
   );
