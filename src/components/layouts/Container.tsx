4769db22e6f9a70f4cb422739bea6e2f32d769f6
nhasan97 <nh6069325@gmail.com>
2025-05-02 08:55:22 +0600

    added padding for xl screen in Container.tsx

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Container = ({ children }: TChildren) => {
   return (
-    <div className="max-w-screen-2xl mx-auto px-5 md:px-8 lg:px-10">
+    <div className="max-w-screen-2xl mx-auto px-5 md:px-8 lg:px-10 xl:px-20">
       {children}
     </div>
   );
