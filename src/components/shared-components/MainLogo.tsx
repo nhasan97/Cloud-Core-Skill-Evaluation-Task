9503498149ad274d53dd3642c3eb09ad138cde89
nhasan97 <nh6069325@gmail.com>
2025-05-03 00:50:11 +0600

    created MainLogo component

---
+++ src/components/shared-components/MainLogo.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+import logo from "../../assets/logo.png";
+
+const MainLogo = () => {
+  return (
+    <div className="text-xl flex items-center">
+      <Image src={logo} alt="Gritly Logo" width={40} height={40} />{" "}
+      <p className="font-medium text-[#202634]">| GRITLY</p>
+    </div>
+  );
+};
+
+export default MainLogo;
