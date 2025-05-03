5e425a88ca6521e160c176807f2559f0ecd87a8f
nhasan97 <nh6069325@gmail.com>
2025-05-03 21:34:47 +0600

    wrapped DisplayProducts component with Container component

--- src/components/modules/home/DisplayProducts.tsx
+++ src/components/modules/home/DisplayProducts.tsx
@@ -7,6 +7,7 @@ import ProductCard from "../../UI/cards/ProductCard";
 import { useAppDispatch, useAppSelector } from "@/src/redux/hook";
 import { fetchProductThunk } from "@/src/redux/slices/productSlice";
 import NoData from "../../shared-components/NoData";
+import Container from "../../layouts/Container";
 
 const DisplayProducts = () => {
   const dispatch = useAppDispatch();
@@ -17,19 +18,21 @@ const DisplayProducts = () => {
   }, [dispatch]);
 
   return (
-    <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
-      {status === "loading" ? (
-        Array.from({ length: 4 }).map((_, index) => (
-          <ProductCardSkeleton key={index} />
-        ))
-      ) : products?.length > 0 ? (
-        products?.map((product: TProduct) => (
-          <ProductCard key={product.unique_id} product={product} />
-        ))
-      ) : (
-        <NoData text="No products found" />
-      )}
-    </div>
+    <Container>
+      <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
+        {status === "loading" ? (
+          Array.from({ length: 4 }).map((_, index) => (
+            <ProductCardSkeleton key={index} />
+          ))
+        ) : products?.length > 0 ? (
+          products?.map((product: TProduct) => (
+            <ProductCard key={product.unique_id} product={product} />
+          ))
+        ) : (
+          <NoData text="No products found" />
+        )}
+      </div>
+    </Container>
   );
 };
 
