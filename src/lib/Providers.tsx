f5478e195e0ba92feb61571c5e141b576d50fa8e
nhasan97 <nh6069325@gmail.com>
2025-05-04 13:51:23 +0600

    installed sonner and added Toaster component in Providers.tsx, created btn-icon-only-style class in button.css

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -4,9 +4,15 @@ import React from "react";
 import { TChildren } from "../types";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import { Toaster } from "sonner";
 
 const Providers = ({ children }: TChildren) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <>
+      <Provider store={store}>{children}</Provider>
+      <Toaster />
+    </>
+  );
 };
 
 export default Providers;
