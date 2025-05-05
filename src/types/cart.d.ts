ec2cb31f67b5f36aea3eff0354bde8ce0504744f
nhasan97 <nh6069325@gmail.com>
2025-05-05 17:37:12 +0600

    created types TCartItem, TCartItemProp, TCartState and TCartContext in cart.d.ts and re-exported every type in index.ts

---
+++ src/types/cart.d.ts
@@ -0,0 +1,26 @@
+export type TCartItem = {
+  id?: number;
+  name: string;
+  price: number;
+  category: string;
+  stock: number;
+  qty: number;
+  image: string;
+};
+
+export type TCartItemProp = { item: TCartItem };
+
+export type TCartState = {
+  cartItems: TCartItem[];
+};
+
+export type TCartContext = {
+  itemsInCart: TCartItem[];
+  itemsInCartCount: number;
+  totalBill: number;
+  // desiredQty: number;
+  // handleEditQtyInProductDetails: (passedQty: number, product: TProduct) => void;
+  handleAddToCart: (product: TProduct, desiredQty?: number) => void;
+  handleEditQty: (editedQty: number, item: TCartItem) => void;
+  handleDeleteCartItem: (id: number) => void;
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,4 @@ export * from "./category";
 export * from "./productImages";
 export * from "./product";
 export * from "./review";
+export * from "./cart";
